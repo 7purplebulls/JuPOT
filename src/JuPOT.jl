afd968a24f45f17dd19a857376860a358a041f14
ShenW123 <shen.w.sw@gmail.com>
2016-02-05 12:56:38 -0500

    export all getters and setters

--- src/JuPOT.jl
+++ src/JuPOT.jl
@@ -4,7 +4,11 @@ import JuMP
 import DataFrames
 # We extend length so it needs to be imported
 import Base.length
-export AbstractModel, optimize, AssetsCollection, SimpleMVO, RobustMVO, getAssetAndReturnsFromCSV
+export AbstractModel, optimize, AssetsCollection, SimpleMVO, RobustMVO, 
+getAssetAndReturnsFromCSV, getReturns, setReturns, getCovariance, getCoVarForAssetPair,
+getVarForAsset, getReturnForAsset, 
+setReturns, setCovariance, setCoVarForAssetPair,
+setVarForAsset, setReturnForAsset
 
 include("core/utils.jl")
 include("core/assetscollection.jl")
